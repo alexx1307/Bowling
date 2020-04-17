07a75480c2e8aaadcdc28f69ce80c914d0264d81
Lukasz <alexx1307@gmail.com>
2020-04-17 20:41:45 +0200

    Base project (after shooting with adjustable force and direction)

---
+++ Assets/Scripts/PyramideBuilder.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PyramideBuilder : MonoBehaviour
+{
+    //obiekt, z którego będziemy budować piramidę (sześcian, może w przyszłości kręgiel)
+    public GameObject prefab;
+
+    //wielkość piramidy, czyli liczba obiektów w podstawie i jednocześnie wysokość piramidy (lizcba pięter)
+    public int size;
+
+    //przesunięcie w ramach pojedynczego wiersza, czyli odstęp między obiektami w wierszu
+    public Vector3 objectsOffset;
+
+    //przesunięcie pomiędzy wierszami, czyli o ile przesunięty jest pierwszy obiekt w wierszu względem pierwszego obiektu poprzedniego wiersza
+    public Vector3 rowOffset;
+
+    //pozycja startowa od której będziemy budować piramidę. Ta zmienna jest PRYWATNA, czyli nie da jej się ustawić w Inspectorze. 
+    //To dlatego, że jej wartość ustalimy ustawiając obiekt zawierający ten skrypt - budowniczego piramidy. Tam gdzie go ustawimy - tam zbuduje się piramida
+    private Vector3 startPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Tutaj właśnie przypisujemy pozycję game objectu (transform.position), jako pozycję startową
+        //Ten transform to jest dokłądnie ten transform, który ma game object budowacza piramid w inspektorze.
+        startPosition = transform.position;
+    }
+
+    void Update()
+    {
+        //jeśli w danej klatce użytkownik nacisnął spację, zbuduj piramidę
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            buildPyramide();
+        }
+    }
+
+    //funkcja budująca piramidę
+    void buildPyramide()
+    {
+        //pętla wchodząca na kolejne rzędy (piętra)
+        for (int row = 0; row < size; row++)
+        {
+            //W danym piętrze uzupełnia się cały wiersz. Z każdym wierszem będzie mniej elementów.
+            //Niech size, który ustawia się w inspectorze wynosi 5
+            //Wtedy w pierwszym rzędzie i zaczyna w 0 a kończy jak osiągnie size - row, czyli 5 - 0 = 5
+            //W drugim rzędzie row wynosi już 1, więc i zaczyna w zero a kończy jak osiątnie 5 - 1 = 4, będzie zatem o jeden element mniej
+            //Ostatni raz ta pętla się wykona kiedy row = 4 (row<size w zewnętrznej pętli)
+            //Wtedy i startuje w 0, a kończy w 5 - 4 = 1, czyli Instantiate wykona się tylko raz dla i = 0, więc w ostatnim rzędzie będzie tylko jeden element
+            for (int i = 0; i < size - row; i++)
+            {
+                //Żeby wyznaczyć pozycję obiektu (kręgla, kostki) dodamy do poczatkowej pozycji jego przesunięcia w ramach wiersza i pomiędzy wirszami
+                //Pierwszy obiekt powstanie, kiedy row = 0 oraz i = 0. Zatem pierwszy oniekt powstanie w miejscu startPosition.
+                //W pierwszym przebiegu całej tej pętli zmienia się tylko "i", a row pozostaje na 0. Dlatego kolejne obiekty będą powstawać w odstępach wynoszących objectsOffset
+                //W kolejnym wierszu, row = 1, dojdzie składnik rowOffset, który powinien określać odstęp między wierszami
+                Vector3 prefabPos = startPosition + row * rowOffset + i * objectsOffset;
+
+                //Stworzenie klona obiektu podanego w inspektorze, na pozycji wyliczonej powyżej, bez zmiany rotacji (ostatni parametr)
+                Instantiate(prefab, prefabPos, Quaternion.identity);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using UnityEngine;
+public class Shooter : MonoBehaviour
+{
+    //prefaby
+    public GameObject ballPrefab;
+    public GameObject forceBar;
+
+    //parametry
+    public float forceOscillation = 1f;
+    public float directionBaseOscillation = 1f;
+    public float forceInfluenceOnDirectionSpeed = 1f;
+    public float forceMultiplier = 1f;
+    public float directionAmplitudeInDegrees = 30f;
+    
+    //stany
+    public bool isPossibleToShoot = true;
+    public bool forceAdjusting = false;
+    public bool directionAdjusting = false;
+
+    //zmienne pomocnicze
+    private float force;
+    private float directionInDegrees;
+    private float timeSinceStartAdjusting;
+    Renderer renderer;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = forceBar.GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        determineState();
+        
+        if (forceAdjusting)
+        {
+            timeSinceStartAdjusting += Time.deltaTime;
+            force = (1f + Mathf.Sin(forceOscillation * timeSinceStartAdjusting)) / 2;
+        }
+
+        if (directionAdjusting)
+        {
+            timeSinceStartAdjusting += Time.deltaTime;
+            float oscillation = directionBaseOscillation + force * forceInfluenceOnDirectionSpeed;
+            float sinValue = Mathf.Sin(oscillation * timeSinceStartAdjusting);
+            directionInDegrees = sinValue * directionAmplitudeInDegrees;
+        }
+
+        DrawForceDirectionArrow();  
+    }
+
+    private void determineState()
+    {
+        if (isPossibleToShoot && Input.GetKeyDown(KeyCode.Space))
+        {
+            isPossibleToShoot = false;
+            forceAdjusting = true;
+            timeSinceStartAdjusting = 0f;
+        }
+        if (forceAdjusting && Input.GetKeyUp(KeyCode.Space))
+        {
+            forceAdjusting = false;
+            directionAdjusting = true;
+            timeSinceStartAdjusting = 0f;
+        }
+        if (directionAdjusting && Input.GetKey(KeyCode.Space))
+        {
+            directionAdjusting = false;
+            isPossibleToShoot = true;
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        GameObject ball = Instantiate(ballPrefab, transform.position, Quaternion.identity);
+        Rigidbody rb = ball.GetComponent<Rigidbody>();
+        Vector3 shootDirection = GetShootDirection();
+
+        rb.velocity = shootDirection * force * forceMultiplier;
+
+        force = 0f;
+        directionInDegrees = 0f;
+    }
+
+    private Vector3 GetShootDirection()
+    {
+        return Quaternion.AngleAxis(directionInDegrees, transform.up) * transform.forward.normalized;
+    }
+
+    private void DrawForceDirectionArrow()
+    {
+       
+
+        if (forceAdjusting || directionAdjusting)
+        {
+            renderer.enabled = true;
+            renderer.material.color = Color.Lerp(Color.green, Color.red, force);
+            forceBar.transform.parent.localScale = new Vector3(1f, 1f, force);
+            forceBar.transform.parent.localEulerAngles = new Vector3(0f,directionInDegrees,0f); 
+        } else
+        {
+            renderer.enabled = false;
+        }
+        
+    }
+}
